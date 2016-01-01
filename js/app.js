c3bd78d797699d540e61817f9f8911189d2632ec
bernicewygo <bernicewygo@gmail.com>
2016-01-01 15:08:51 -0500

    v2 changes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
 $(function () { 
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 		// init
 		var controller = new ScrollMagic.Controller();
 		// get all slides
 		var slides = document.querySelectorAll("section.panel");
 
 		// create scene for every slide
-		for (var i=0; i<slides.length; i++) {
-			new ScrollMagic.Scene({
-					triggerElement: slides[i],
-					triggerHook: 'onLeave',
-					duration: "100%"
+		// for (var i=0; i<slides.length; i++) {
+		// 	new ScrollMagic.Scene({
+		// 			triggerElement: slides[i],
+		// 			triggerHook: 'onLeave',
 
-				})
-				.setPin(slides[i])
-				.addTo(controller);
-		}
+		// 		})
+		// 		.setPin(slides[i])
+		// 		.addTo(controller);
+		// }
 
 		var scene = new ScrollMagic.Scene({
             triggerElement: "#animate1",
